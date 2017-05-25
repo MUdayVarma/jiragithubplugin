ed9d685e7f6b51eabd7653a926419b0e84e85907
Uday Varma <Uday_Varma@ad.infosys.com>
2017-05-25 17:12:02 +0530

    Push to Remote Repository

---
+++ helloPython.py
@@ -0,0 +1,19 @@
+print("Hello Python. I am Using Git for you")
+myName = input("What is your name? ")
+print(myName)
+
+myVar = 'Python looks interesting'
+print (myVar)
+
+myNum = input("Enter a Number ")
+
+if(myName == "uday" and myNum != '1'):
+	print ("Practice Python as much as possible")
+elif(myName == "Anaony"):
+	print ("What do you do?")
+else:
+	print("Give your intro please")
+	
+	
+	
+
